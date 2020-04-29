021830dc9dd98b219b47096c33e3fef0f9524f74
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-04-29 12:04:45 +0530

    setup file while ansible setup

---
+++ scripts/ansiblesetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "User ID is : ${1}" >> log.txt
+
+clonerepo(){
+cd /home/${1}/ 
+wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/setup_ansible.sh 
+
+sudo chown -R ${1}:${1} /home/${1}/setup_ansible.sh
+#sudo chown -R azureadmin:azureadmin /home/azureadmin/install_wp.sh
+}
+
+clonerepo ${1} >> log.txt
