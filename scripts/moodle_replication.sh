dc7cf2f2abe3039ac81d5d638790689adbabebe7
tmdasif91 <tmdasif91@gmail.com>
2020-05-22 13:12:03 +0530

    added moodle scripts

--- scripts/moodle_replication.sh
+++ scripts/moodle_replication.sh
@@ -17,7 +17,6 @@ configuring_certs() {
     sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${repli_certs}/nginx.key -out ${repli_certs}/nginx.crt -subj "/C=US/ST=WA/L=Redmond/O=IT/CN=${1}"
     sudo chown www-data:www-data ${repli_certs}/nginx.*
     sudo chmod 400 ${repli_certs}/nginx.*
-
 }
 linking_data_location() {
     echo "linking func"
