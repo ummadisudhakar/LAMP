afd1dc0328cdf2c99bfecdfa6fc75959dd211cf6
Asif iTalent <60592188+asift91@users.noreply.github.com>
2020-05-29 18:59:10 +0530

    added ownership for  replica_path
    added ownership for replicat_path

--- scripts/moodle_replication.sh
+++ scripts/moodle_replication.sh
@@ -15,6 +15,7 @@ wp_content=wp-content/uploads
 change_location() {
     sudo mkdir ${replica_path}
     sudo cp -rf ${webroot}/moodle/* ${replica_path}
+    sudo chown -R www-data:www-data ${replica_path}
 }
 configuring_certs() {
     sudo mkdir ${replica_certs}
