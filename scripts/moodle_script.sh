2d7a6e52909d1febdb1247c27810425e7c6b868d
Asif iTalent <60592188+asift91@users.noreply.github.com>
2020-06-09 17:49:16 +0530

    updated comments and removed white spaces

--- scripts/moodle_script.sh
+++ scripts/moodle_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# moodle_script will be replacing the inputs dynamically
-# moodle_script will be executing the playbook 
+# Install ansible server, SVN and configure the host VM IP (controller VM IP) 
+# It will update groups_var/all file in playbook with the user inputs dynamically
+# It will execute ansible playbook for installing Moodle in host VM (controller VM)
 
 log_path=/home/${3}/var.txt
 vars_path=/home/${3}/moodle/group_vars/all
@@ -25,7 +26,7 @@ install_svn() {
 }
 run_moodle_playbook() {
     cd /home/${3}
-    svn checkout https://github.com/ummadisudhakar/LAMP/trunk/scripts/ansiblePlaybook/moodle
+    svn checkout https://github.com/Azure/LAMP/trunk/scripts/ansiblePlaybook/moodle
     sudo sed -i "s~vm_ip: IP~vm_ip: ${1}~" ${vars_path}
     sudo sed -i "s~vm_password: password~vm_password: ${2}~" ${vars_path}
     sudo sed -i "s~user_name: azusername~user_name: ${3}~" ${vars_path}
@@ -40,11 +41,13 @@ run_moodle_playbook() {
     ansible-playbook /home/${3}/moodle/playbook.yml -i /etc/ansible/hosts -u ${3}
 }
 
+# Disable strict host key checking to configure host VM IP  (controller VM IP)
 sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config >>${log_path}
 sudo systemctl restart ssh
 setup_ansible
 configure_ansible_inventory ${1} ${4}
 install_svn
 run_moodle_playbook ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} >>${log_path}
+# Enable strict host key checking
 sudo sed -i "s~   StrictHostKeyChecking no~#   StrictHostKeyChecking ask~" /etc/ssh/ssh_config >>${log_path}
 sudo systemctl restart ssh
