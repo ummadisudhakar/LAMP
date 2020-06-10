0ac5080ff49a1c2ff6479ee714e17c41a6868777
Asif iTalent <60592188+asift91@users.noreply.github.com>
2020-06-10 11:24:02 +0530

    Updated webroot
    reading webroot from the ansible-playbook

--- scripts/woocommerce.sh
+++ scripts/woocommerce.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Woocommerce plugin will be downloaded and copied to plugins directory of wordpress
+# Woocommerce plugin will be downloaded and copied to plugins directory of Wordpress
 
 wooco_URL=${1} 
 wooco_path=/home/${2}
-web_root=/var/www/html
+web_root=${4}
 wooco_version=${3}
-wooco_direc_name=downloads.wordpress.org
+wooco_dir_name=downloads.wordpress.org
 wooco_plugin_path=plugin/woocommerce
 
 downloadwoocommerce(){
@@ -15,10 +15,10 @@ downloadwoocommerce(){
 
 extractfile(){
   sudo apt install unzip
-  sudo unzip ${wooco_path}/${wooco_direc_name}/${wooco_plugin_path}.${wooco_version}.zip
+  sudo unzip ${wooco_path}/${wooco_dir_name}/${wooco_plugin_path}.${wooco_version}.zip
   sudo cp -rf ${wooco_path}/woocommerce ${web_root}/wordpress/wp-content/plugins/
   sudo rm -rf ${wooco_path}/woocommerce
-  sudo rm -rf {wooco_path}/${wooco_direc_name}/
+  sudo rm -rf {wooco_path}/${wooco_dir_name}/
 }
 
 downloadwoocommerce 
